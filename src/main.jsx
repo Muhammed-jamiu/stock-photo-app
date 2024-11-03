39dc9264af28e3169fba80e010b5186bec4e3b3c
muhammed-jamiu <anatemuhammed@gmail.com>
2024-11-03 11:48:33 +0100

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './new/index.css';
+import App from "./new/App.js"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+<App/>
   </StrictMode>,
 )
